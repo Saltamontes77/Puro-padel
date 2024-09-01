6d8738106ba9e45afc45d319a1ca0f5552a1d721
Saltamontes77 <leorivero7@live.com>
2024-09-01 11:16:10 -0300

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -5,3 +5,12 @@ function mtscuadrados (largo, ancho) {
 
 mtscuadrados (230,26)
 mtscuadrados (34, 54)
+
+
+function area (pi,radio2) {
+    let area = pi * radio2;
+    console.log(area + "m2");
+}
+
+area (37,25)
+area (44,33)
